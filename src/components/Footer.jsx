d64dc2513749d6b770efecc3be38d6d4ab73bed8
Sahan Kalhara <142345492+SkDev22@users.noreply.github.com>
2025-06-20 22:37:49 +0530

    Update Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
   return (
-    <div className="flex justify-between pt-10 pb-2 px-32 text-pretty bg-[#0D1B2A]">
+    <div className="flex justify-between pt-10 pb-2 px-20 text-pretty bg-[#0D1B2A]">
       <div className="flex">
-        <h1 className="text-2xl text-white font-semibold font-family-EduNSWACTCursive">
-          SkDev
-        </h1>
+        <Link to="/">
+          <h1 className="text-2xl text-white font-semibold font-family-EduNSWACTCursive">
+            SkDev
+          </h1>
+        </Link>
       </div>
       <div className="flex text-gray-500 font-family-Poppins">
-        <h1>© 2025 Sahan Kalhara. All rights reserved.</h1>
+        <h1>
+          © {new Date().getFullYear()} Sahan Kalhara. All rights reserved.
+        </h1>
       </div>
     </div>
   );
